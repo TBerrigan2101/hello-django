492a88170f0307a020d128aad336381ef16808f3
TBerrigan2101 <tberrigan2101@gmail.com>
2023-07-20 20:23:56 +0000

    Save but app not workking. Need to revert to older code

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,15 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-=hgl61rh7-w@z7pq#21^q2l^^4$%72x+i_h2wpk%b)0(9rjnep')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-if development:
-    ALLOWED_HOSTS = ['localhost']
-else:
-    ALLOWED_HOSTS = ['8000-tberrigan21-hellodjango-at1wdc91ype.ws-eu102.gitpod.io']
+ALLOWED_HOSTS = ['8000-tberrigan21-hellodjango-at1wdc91ype.ws-eu102.gitpod.io']
 
 # Application definition
 
